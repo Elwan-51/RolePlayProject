2525118ca5845252eaaf587ca990f179ab3e8713
Elwan-51 <78788340+Elwan-51@users.noreply.github.com>
2022-04-25 01:40:53 +0200

    Update database.py

--- APP/database.py
+++ APP/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://elwan:ez93oq41&*@nas.asylium.org/roleplaytest"
+SQLALCHEMY_DATABASE_URL = "postgresql://elwan:@nas.asylium.org/roleplaytest"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL)
